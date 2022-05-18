3d229df0bf56c91a015a4734b06ccd8d40b2edc1
user_pling <501492630@qq.com>
2022-05-18 16:39:35 +0800

    项目初始化-调整初始目录解构

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,16 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    // '@vue/standard'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "space-before-function-paren":0,
+    'indent':'off',
+    "vue/multi-word-component-names":"off",
   }
 }
